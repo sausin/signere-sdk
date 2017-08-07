69bad138c5c0dcf9f88f5c87160defc6a3c9a306
Saurabh Singhvi <saurabh@getmail.no>
2017-08-07 19:52:50 +0200

    Initiate work on controllers

---
+++ src/Http/Controllers/SignereIdController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sausin\Signere\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class SignereIdController extends Controller
+{
+}
