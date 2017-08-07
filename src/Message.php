31ba24ed2bab563f21043b8ba46fb7150a4f07f3
Saurabh Singhvi <saurabh@getmail.no>
2017-08-07 20:16:04 +0200

    Message completed

--- src/Message.php
+++ src/Message.php
@@ -14,7 +14,7 @@ class Message
     protected $client;
 
     /** The URI of the action */
-    const URI = 'https://api.signere.no/api/Message/Document/';
+    const URI = 'https://api.signere.no/api/Message';
 
     /**
      * Instantiate the class.
@@ -27,22 +27,119 @@ class Message
     }
 
     /**
-     * Get a list of messages sent for the given document ID
+     * Get a list of messages sent for the given document ID.
      *
-     * @param  string $id
+     * @param  string $messageId
      * @return json
      */
-    public static function get(string $id)
+    public static function get(string $messageId)
     {
         // get the headers for this request
         $headers = Headers::make('GET');
 
         // make the URL for this request
-        $url = self::URI . $id;
+        $url = sprintf('%s/%s', self::URI, $messageId);
 
         // get the response
         $response = $this->client->get($url, [
             'headers' => $headers
+        ]);
+
+        // return the response
+        return $response;
+    }
+
+    /**
+     * Retrieves a list of all the messages that 
+     * are sent for the given document.
+     * 
+     * @param  string $documentId
+     * @return Object
+     */
+    public static function all(string $documentId)
+    {
+        // get the headers for this request
+        $headers = Headers::make('GET');
+
+        // make the URL for this request
+        $url = sprintf('%s/Document/%s', self::URI, $documentId);
+
+        // get the response
+        $response = $this->client->get($url, [
+            'headers' => $headers
+        ]);
+
+        // return the response
+        return $response;
+    }
+
+    /**
+     * Sends a message to the signees of a given document.
+     * 
+     * @param  array  $body
+     * @return Object
+     */
+    public static function sendMessage(array $body)
+    {
+        // get the headers for this request
+        $headers = Headers::make('POST');
+
+        // make the URL for this request
+        $url = sprintf('%s/Message', self::URI);
+
+        // get the response
+        $response = $this->client->post($url, [
+            'headers' => $headers,
+            'json' => $body
+        ]);
+
+        // return the response
+        return $response;
+    }
+
+    /**
+     * Sends a new message to the Signeeref if the first one failed.
+     * 
+     * @param  array  $body
+     * @return Object
+     */
+    public static function sendNewMessage(array $body)
+    {
+        // get the headers for this request
+        $headers = Headers::make('PUT');
+
+        // make the URL for this request
+        $url = sprintf('%s/Message/SendNewDocumentMessage', self::URI);
+
+        // get the response
+        $response = $this->client->post($url, [
+            'headers' => $headers,
+            'json' => $body
+        ]);
+
+        // return the response
+        return $response;
+    }
+
+    /**
+     * Sends a message to an external person with a link/URL
+     * to view a document.
+     * 
+     * @param  array  $body
+     * @return Object
+     */
+    public static function sendExternalMessage(array $body)
+    {
+        // get the headers for this request
+        $headers = Headers::make('POST');
+
+        // make the URL for this request
+        $url = sprintf('%s/Message/SendExternalMessage', self::URI);
+
+        // get the response
+        $response = $this->client->post($url, [
+            'headers' => $headers,
+            'json' => $body
         ]);
 
         // return the response
