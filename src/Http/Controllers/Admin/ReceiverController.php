add6d85965a86cacad29f8026fc5ae530f600fc7
Saurabh Singhvi <saurabh@getmail.no>
2017-08-07 20:16:14 +0200

    More controllers initialized

---
+++ src/Http/Controllers/Admin/ReceiverController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sausin\Signere\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class ReceiverController extends Controller
+{
+}
