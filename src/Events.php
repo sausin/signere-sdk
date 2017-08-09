5c910794066afbe3a9d9bd816b46b02c51a9331b
Saurabh Singhvi <saurabh@getmail.no>
2017-08-09 23:39:56 +0200

    Added and tested events

---
+++ src/Events.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Sausin\Signere;
+
+use GuzzleHttp\Client;
+
+class Events
+{
+    /** @var \Guzzle\HttpClient */
+    protected $client;
+
+    /** @var Headers */
+    protected $headers;
+
+    /** The URI of the action */
+    const URI = 'https://api.signere.no/api/events/encryptionkey';
+
+    /**
+     * Instantiate the class.
+     *
+     * @param \GuzzleHttp\Client $client
+     */
+    public function __construct(Client $client, Headers $headers)
+    {
+        $this->client = $client;
+        $this->headers = $headers;
+    }
+
+    /**
+     * Returns the EventsQueue encryptionKey as a base64 encoded string.
+     *
+     * @return Object
+     */
+    public function getEncryptionKey()
+    {
+        // make the URL for this request
+        $url = self::URI;
+
+        // get the headers for this request
+        $headers = $this->headers->make('GET', $url);
+
+        // get the response
+        $response = $this->client->get($url, [
+            'headers' => $headers
+        ]);
+
+        // return the response
+        return $response;
+    }
+}
---
+++ tests/EventsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sausin\Signere\Tests;
+
+use Mockery as m;
+use Sausin\Signere\Events;
+use Sausin\Signere\Headers;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Support\Facades\Config;
+
+class EventsTest extends TestCase
+{
+    use MakeClient;
+
+    public function setUp()
+    {
+        $this->headers = m::mock(Headers::class);
+        $this->uri = 'https://api.signere.no/api/events/encryptionkey';
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    /** @test */
+    public function it_can_get_encryption_key()
+    {
+        $detail = 'sdl;fksdl;fksldfsdlkfsdl;kfs;ldk';
+
+        $url = $this->uri;
+
+        $this->headers->shouldReceive('make')->once()->withArgs(['GET', $url])->andReturn([]);
+
+        $event = new Events($this->makeClient($detail), $this->headers);
+        $response = $event->getEncryptionKey();
+
+        $this->assertEquals($detail, $response->getBody()->getContents());
+    }
+}
