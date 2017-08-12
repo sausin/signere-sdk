ef5b0c30d89e9b6fb50da01c9a6989086d90d320
Saurabh Singhvi <saurabh@getmail.no>
2017-08-12 19:02:11 +0200

    Validation trait added

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -4,9 +4,12 @@ namespace Sausin\Signere\Http\Controllers;
 
 use Sausin\Signere\Http\Middleware\Authenticate;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class Controller extends BaseController
 {
+    use ValidatesRequests;
+    
     /**
      * Create a new controller instance.
      *
