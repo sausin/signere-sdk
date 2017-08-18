f8187ca274f823a5b8c32274a922bc23b3ca1080
Saurabh Singhvi <saurabh@getmail.no>
2017-08-18 21:29:05 +0200

    Document provider tested

--- src/Http/Controllers/Admin/DocumentProviderController.php
+++ src/Http/Controllers/Admin/DocumentProviderController.php
@@ -4,6 +4,7 @@ namespace Sausin\Signere\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use Sausin\Signere\DocumentProvider;
+use Illuminate\Support\Facades\Config;
 use Sausin\Signere\Http\Controllers\Controller;
 
 class DocumentProviderController extends Controller
@@ -57,10 +58,65 @@ class DocumentProviderController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        //
-        //
-        //
+        $this->validate($request, [
+            'address1' => 'bail|required|string|min:1|max:255',
+            'address2' => 'bail|nullable|string|min:1|max:255',
+            'city' => 'bail|required|string|min:1|max:255',
+            'country' => 'bail|nullable|string|min:1|max:255',
+            'billing_plan' => 'bail|nullable|string|in:Small,Medium,Large',
+            'postcode' => [
+                'bail',
+                'required',
+                'regex:/^\d{4}$/i'
+            ],
+            'company_email' => 'bail|required|email|max:255',
+            'company_phone' => [
+                'bail',
+                'required',
+                'string',
+                'regex:/^\+47\d{8}$/i'
+            ],
+            'dealer_id' => 'bail|required|string|size:36',
+            'legal_contact_email' => 'bail|required|email|max:255',
+            'legal_contact_name' => 'bail|required|string|min:1|max:255',
+            'legal_contact_phone' => [
+                'bail',
+                'required',
+                'string',
+                'regex:/^\+47\d{8}$/i'
+            ],
+            'mva_number' => 'bail|required|numeric|min:800000000|max:999999999',
+            'name' => 'bail|required|string|min:1|max:255'
+        ]);
+
+        // this is used to only set the keys which have been sent in
+        $useKeys = [
+            'address1' => 'BillingAddress1',
+            'address2' => 'BillingAddress2',
+            'city' => 'BillingCity',
+            'country' => 'BillingCountry',
+            'billing_plan' => 'BillingPlan',
+            'postcode' => 'BillingPostalCode',
+            'company_email' => 'CompanyEmail',
+            'company_phone' => 'CompanyPhone',
+            'dealer_id' => 'DealerId',
+            'legal_contact_email' => 'LegalContactEmail',
+            'legal_contact_name' => 'LegalContactName',
+            'legal_contact_phone' => 'LegalContactPhone',
+            'mva_number' => 'MvaNumber',
+            'name' => 'Name',
+        ];
+
+        // check which keys are available in the request
+        $available = array_intersect(array_keys($useKeys), array_keys($request->all()));
+
+        $body = [];
+
+        // set the body up
+        foreach ($available as $use) {
+            $body[$useKeys[$use]] = $request->$use;
+        }
+
         return $this->dp->create($body)
                 ->getBody()
                 ->getContents();
@@ -75,10 +131,65 @@ class DocumentProviderController extends Controller
      */
     public function update(Request $request)
     {
-        //
-        //
-        //
-        //
+        $this->validate($request, [
+            'address1' => 'bail|sometimes|string|min:1|max:255',
+            'address2' => 'bail|sometimes|nullable|string|min:1|max:255',
+            'city' => 'bail|sometimes|string|min:1|max:255',
+            'country' => 'bail|sometimes|nullable|string|min:1|max:255',
+            'billing_plan' => 'bail|sometimes|nullable|string|in:Small,Medium,Large',
+            'postcode' => [
+                'bail',
+                'sometimes',
+                'regex:/^\d{4}$/i'
+            ],
+            'company_email' => 'bail|sometimes|email|max:255',
+            'company_phone' => [
+                'bail',
+                'sometimes',
+                'string',
+                'regex:/^\+47\d{8}$/i'
+            ],
+            'dealer_id' => 'bail|sometimes|string|size:36',
+            'legal_contact_email' => 'bail|sometimes|email|max:255',
+            'legal_contact_name' => 'bail|sometimes|string|min:1|max:255',
+            'legal_contact_phone' => [
+                'bail',
+                'sometimes',
+                'string',
+                'regex:/^\+47\d{8}$/i'
+            ],
+            'mva_number' => 'bail|sometimes|numeric|min:800000000|max:999999999',
+            'name' => 'bail|sometimes|string|min:1|max:255'
+        ]);
+
+        // this is used to only set the keys which have been sent in
+        $useKeys = [
+            'address1' => 'BillingAddress1',
+            'address2' => 'BillingAddress2',
+            'city' => 'BillingCity',
+            'country' => 'BillingCountry',
+            'billing_plan' => 'BillingPlan',
+            'postcode' => 'BillingPostalCode',
+            'company_email' => 'CompanyEmail',
+            'company_phone' => 'CompanyPhone',
+            'dealer_id' => 'DealerId',
+            'legal_contact_email' => 'LegalContactEmail',
+            'legal_contact_name' => 'LegalContactName',
+            'legal_contact_phone' => 'LegalContactPhone',
+            'mva_number' => 'MvaNumber',
+            'name' => 'Name',
+        ];
+
+        // check which keys are available in the request
+        $available = array_intersect(array_keys($useKeys), array_keys($request->all()));
+
+        $body = [];
+
+        // set the body up
+        foreach ($available as $use) {
+            $body[$useKeys[$use]] = $request->$use;
+        }
+
         return $this->dp->update($body)
                 ->getBody()
                 ->getContents();
---
+++ tests/Controllers/Admin/DocumentProviderControllerTest.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace Sausin\Signere\Tests\Controllers\Admin;
+
+use Mockery as m;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use Sausin\Signere\DocumentProvider;
+use Illuminate\Support\Facades\Config;
+use Sausin\Signere\Tests\Controllers\Fakes\User;
+use Sausin\Signere\Tests\Controllers\AbstractControllerTest;
+
+class DocumentProviderControllerTest extends AbstractControllerTest
+{
+    public function tearDown()
+    {
+        parent::tearDown();
+        
+        m::close();
+    }
+    
+    /** @test */
+    public function an_admin_can_get_account_details()
+    {
+        $dp = m::mock(DocumentProvider::class);
+
+        // make a check on the object if the method actually exists
+        // this is to be certain that code changes in the original
+        // class will not lead to this test passing by mistake
+        $this->assertTrue(method_exists($dp, 'getProviderAccount'));
+
+        $dp->shouldReceive('getProviderAccount')
+                ->once()
+                ->with('id')
+                ->andReturn(new Response(200, [], ''));
+
+        $this->app->instance(DocumentProvider::class, $dp);
+
+        $this->actingAs(new User)
+            ->json('GET', '/signere/admin/provider')
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function an_admin_can_get_usage_details()
+    {
+        $dp = m::mock(DocumentProvider::class);
+
+        // make a check on the object if the method actually exists
+        // this is to be certain that code changes in the original
+        // class will not lead to this test passing by mistake
+        $this->assertTrue(method_exists($dp, 'getUsage'));
+
+        $dp->shouldReceive('getUsage')
+                ->once()
+                ->with('id')
+                ->andReturn(new Response(200, [], ''));
+
+        $this->app->instance(DocumentProvider::class, $dp);
+
+        $this->actingAs(new User)
+            ->json('GET', '/signere/admin/provider/usage')
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function an_admin_can_create_a_provider()
+    {
+        $dp = m::mock(DocumentProvider::class);
+
+        // make a check on the object if the method actually exists
+        // this is to be certain that code changes in the original
+        // class will not lead to this test passing by mistake
+        $this->assertTrue(method_exists($dp, 'create'));
+
+        $body1 = [
+            'address1' => $address1 = str_random(10),
+            'address2' => $address2 = str_random(10),
+            'city' => $city = str_random(10),
+            'country' => $country = str_random(10),
+            'billing_plan' => $billing_plan = 'Small',
+            'postcode' => $postcode = rand(1000, 9999),
+            'company_email' => $company_email = 'u@two.com',
+            'company_phone' => $company_phone = '+4712345678',
+            'dealer_id' => $dealer_id = str_random(36),
+            'legal_contact_email' => $legal_contact_email = 'post@rand.ax',
+            'legal_contact_name' => $legal_contact_name = str_random(10),
+            'legal_contact_phone' => $legal_contact_phone = '+4712345678',
+            'mva_number' => $mva_number = rand(800000000, 999999999),
+            'name' => $name = str_random(10),
+        ];
+
+        $body2 = [
+            'BillingAddress1' => $address1,
+            'BillingAddress2' => $address2,
+            'BillingCity' => $city,
+            'BillingCountry' => $country,
+            'BillingPlan' => $billing_plan,
+            'BillingPostalCode' => $postcode,
+            'CompanyEmail' => $company_email,
+            'CompanyPhone' => $company_phone,
+            'DealerId' => $dealer_id,
+            'LegalContactEmail' => $legal_contact_email,
+            'LegalContactName' => $legal_contact_name,
+            'LegalContactPhone' => $legal_contact_phone,
+            'MvaNumber' => $mva_number,
+            'Name' => $name,
+        ];
+
+        $dp->shouldReceive('create')
+                ->once()
+                ->with($body2)
+                ->andReturn(new Response(200, [], ''));
+
+        $this->app->instance(DocumentProvider::class, $dp);
+
+        $this->actingAs(new User)
+            ->json('POST', '/signere/admin/provider', $body1)
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function an_admin_can_create_a_provider_with_only_required_details()
+    {
+        $dp = m::mock(DocumentProvider::class);
+
+        // make a check on the object if the method actually exists
+        // this is to be certain that code changes in the original
+        // class will not lead to this test passing by mistake
+        $this->assertTrue(method_exists($dp, 'create'));
+
+        $body1 = [
+            'address1' => $address1 = str_random(10),
+            'city' => $city = str_random(10),
+            'postcode' => $postcode = rand(1000, 9999),
+            'company_email' => $company_email = 'u@two.com',
+            'company_phone' => $company_phone = '+4712345678',
+            'dealer_id' => $dealer_id = str_random(36),
+            'legal_contact_email' => $legal_contact_email = 'post@rand.ax',
+            'legal_contact_name' => $legal_contact_name = str_random(10),
+            'legal_contact_phone' => $legal_contact_phone = '+4712345678',
+            'mva_number' => $mva_number = rand(800000000, 999999999),
+            'name' => $name = str_random(10),
+        ];
+
+        $body2 = [
+            'BillingAddress1' => $address1,
+            'BillingCity' => $city,
+            'BillingPostalCode' => $postcode,
+            'CompanyEmail' => $company_email,
+            'CompanyPhone' => $company_phone,
+            'DealerId' => $dealer_id,
+            'LegalContactEmail' => $legal_contact_email,
+            'LegalContactName' => $legal_contact_name,
+            'LegalContactPhone' => $legal_contact_phone,
+            'MvaNumber' => $mva_number,
+            'Name' => $name,
+        ];
+
+        $dp->shouldReceive('create')
+                ->once()
+                ->with($body2)
+                ->andReturn(new Response(200, [], ''));
+
+        $this->app->instance(DocumentProvider::class, $dp);
+
+        $this->actingAs(new User)
+            ->json('POST', '/signere/admin/provider', $body1)
+            ->assertStatus(200);
+    }
+
+    /** @test */
+    public function an_admin_can_update_a_provider_with_only_some_details()
+    {
+        $dp = m::mock(DocumentProvider::class);
+
+        // make a check on the object if the method actually exists
+        // this is to be certain that code changes in the original
+        // class will not lead to this test passing by mistake
+        $this->assertTrue(method_exists($dp, 'update'));
+
+        $body1 = [
+            'address1' => $address1 = str_random(10),
+            'city' => $city = str_random(10),
+            'legal_contact_email' => $legal_contact_email = 'post@rand.ax',
+            'legal_contact_phone' => $legal_contact_phone = '+4712345678',
+            'name' => $name = str_random(10),
+        ];
+
+        $body2 = [
+            'BillingAddress1' => $address1,
+            'BillingCity' => $city,
+            'LegalContactEmail' => $legal_contact_email,
+            'LegalContactPhone' => $legal_contact_phone,
+            'Name' => $name,
+        ];
+
+        $dp->shouldReceive('update')
+                ->once()
+                ->with($body2)
+                ->andReturn(new Response(200, [], ''));
+
+        $this->app->instance(DocumentProvider::class, $dp);
+
+        $this->actingAs(new User)
+            ->json('PATCH', '/signere/admin/provider', $body1)
+            ->assertStatus(200);
+    }
+}
